d081a7a046c2cb7b47971e315f2d2c2030b421df
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-06-22 21:07:02 +0530

    Add files via upload

---
+++ num_pattern_55.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+     int n;
+     printf("Enter the no of rows: ");
+     scanf("%d", &n);
+     for (int i = 1; i <= n; i++)
+     {
+          for (int j = 1; j <= i; j++)
+          {
+               printf("%d", j);
+          }
+          printf("\n");
+     }
+     // Print lower part of the pattern
+     //     for(i=n-1; i>=1; i--)
+     //     {
+     //         for(j=1; j<=i; j++)
+     //         {
+     //             printf("%d", j);
+     //         }
+
+     //         printf("\n");
+     //     }
+     for (int i = 1; i <= n; i++)
+     {
+          for (int j = 1; j <= n - i; j++)
+          {
+               printf("%d", j);
+          }
+          printf("\n");
+     }
+     return 0;
+}
